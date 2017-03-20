cb7a9258ff203cb11d4059656dd49ce7f0460aad
kocharshaivi19 <sk6435@nyu.edu>
2017-03-20 03:43:47 -0400

    Removed unwanted comments

--- TweetMap/urls.py
+++ TweetMap/urls.py
@@ -1,15 +1,3 @@
-# TweetsMap/urls.py
-from django.conf.urls import include, url
-from . import views
-
-# from TweetMap.models import Rental
-
-# urlpatterns = [
-# 	url(r'^TweetMap', views.index, name='index'),
-# 	url(r'^contact', views.contact, name='contact'),
-#
-# ]
-
 from django.conf import settings
 from django.conf.urls import url
 from django.conf.urls.static import static
